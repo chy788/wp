f983877404fa4a23304e1c154677555a22314a3d
chy788 <chy7888@163.com>
2014-06-25 15:00:43 +0800

    explain

--- wordpress/wp-content/themes/twentytwelve/index.php
+++ wordpress/wp-content/themes/twentytwelve/index.php
@@ -57,7 +57,7 @@ $categories=get_categories($args);
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
--->
+
 			<?php twentytwelve_content_nav( 'nav-below' ); ?>
 
 		<?php else : ?>
@@ -73,8 +73,9 @@ $categories=get_categories($args);
 
 				<div class="entry-content">
 					<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwelve' ), admin_url( 'post-new.php' ) ); ?></p>
-				</div><!-- .entry-content -->
-
+				</div>-->
+				<!-- .entry-content -->
+<!--
 			<?php else :
 				// Show the default message to everyone else.
 			?>
@@ -85,12 +86,16 @@ $categories=get_categories($args);
 				<div class="entry-content">
 					<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'twentytwelve' ); ?></p>
 					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
+				</div>-->
+				<!-- .entry-content -->
+				<!--
 			<?php endif; // end current_user_can() check ?>
 
-			</article><!-- #post-0 -->
+			</article>
+			 -->
+			 <!-- #post-0 -->
 
-		<?php endif; // end have_posts() check ?>
+		<!--<?php endif; // end have_posts() check ?>-->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
