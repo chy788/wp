0892f1a5e2bb2455860555bf984078a62e285ee6
chy788 <chy7888@163.com>
2014-07-03 17:13:03 +0800

    baidu statistic js code

--- wordpress/wp-content/themes/twentytwelve/footer.php
+++ wordpress/wp-content/themes/twentytwelve/footer.php
@@ -22,5 +22,9 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb5305e1362f85708644cd8864cd1d3df' type='text/javascript'%3E%3C/script%3E"));
+</script>
 </body>
 </html>
