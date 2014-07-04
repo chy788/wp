176e0a8aac9d6e02e1c0256e4bdead7aff16ded0
chy788 <chy7888@163.com>
2014-07-04 11:33:08 +0800

    new.gif

--- wordpress/wp-content/themes/twentytwelve/index.php
+++ wordpress/wp-content/themes/twentytwelve/index.php
@@ -48,7 +48,12 @@ $categories=get_categories($args);
 			?>
 			<img src='http://www.shoucaihuo.com/wp/wordpress/hot.gif' >
 			<?php
-		}
+		} else if(get_the_id() == 67)
+		{
+			?>
+			<img src='http://www.shoucaihuo.com/wp/wordpress/new.gif' >
+			<?php
+		} 
 			?>
 			</h3>
 			</br>
